7a0eee620c76288588b2e518ddfdade80aee45fd
Grasi Eduarda <grasieduarda775@gmail.com>
2022-02-17 13:39:24 -0300

    BUG resolvdido

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,33 +3,25 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def def new
+  def new
     @product = Product.new
   end
-  
+
   def create
     @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { render :index, notice: 'Product created.' }
+        format.html { redirect_to products_path, notice: 'Product created.' }
       else
         format.html { render :new }
       end
     end
   end
 
-  respond_to do |format|
-    if @restaurant.save
-      format.html { redirect_to restaurant_url(@restaurant), notice: "Restaurant was successfully created." }
-      format.json { render :show, status: :created, location: @restaurant }
-    else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-    end
-  end
-  
+  private
+
   def product_params
-    params.require(:product).permit(:name, :value, :Restaurant_id)
+    params.require(:product).permit(:name, :value, :restaurant_id)
   end
 end
